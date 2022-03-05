bbeed2197f56fce7ef52c9809ad9bed4aadd4693
Jayaranjini <jaya.ranjini03@gmail.com>
2022-03-05 21:51:13 +0530

    Sas13

---
+++ src/org/overload/Overload.java
@@ -0,0 +1,27 @@
+package org.overload;
+
+public class Overload {
+	
+	private void emp(int age) {
+		System.out.println("emp age is :");
+
+	}
+
+	private void emp(String FN,String LN) {
+         System.out.println("emp firstname is "+FN +"\n"+ "emp lastname is "+LN);
+	}
+
+    private void emp(float phonenumber,String address,String empid) {
+         System.out.println("emp numer is :"+phonenumber+"\n"+"emp address is :"+address+"\n"+"emp id is :"+empid);
+	}
+
+public static void main(String[] args) {
+	Overload a=new Overload();
+	a.emp(30);
+	a.emp("sasi", "kumar");
+	a.emp(5846356465f, "OMR Thoraipakkam", "jayaranjini.oim@gmail.com");
+	a.emp(45);
+	a.emp("geetha", "kumari");
+}
+
+}
